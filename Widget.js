b414107cdffcb096b5e1ed06e577ff020ce8d1c3
Brayan Guerrero <brayang26_@hotmail.com>
2016-10-27 11:35:47 -0500

    Uncaught TypeError: this._onTransparencyChanged is not a function

--- Widget.js
+++ Widget.js
@@ -26,27 +26,33 @@ define([
 		horizontalSlider : null,
 		//this property is set by the framework when widget is loaded.
 		name : 'Slider',
+		_vectorial : null,
+		_baseAerea : null,
 
 		//methods to communication with app container:
 
 		postCreate : function () {
 			this.inherited(arguments);
+			this._vectorial = this.map.getLayer(this.map.layerIds[1]);
+			this._baseAerea = this.map.getLayer(this.map.layerIds[2]);
 			console.log('postCreate');
 		},
 
 		startup : function () {
 			this.inherited(arguments);
 			this.horizontalSlider = new HorizSlider({
-					minimum: 0,
-					maximum: 1,
-					intermediateChanges: true,
-					onChange: function(value){
-						console.log('slider value: '+value);
+					minimum : 0,
+					maximum : 1,
+					intermediateChanges : true,
+					onChange : function (value) {
+						console.log('slider value: ' + value);
+						this._onTransparencyChanged(value);
+						console.log('opacity changed');
 					}
 				}, this.sliderNode);
 			this.horizontalSlider.startup();
 			new HorzRuleLabels({
-				container: "bottomDecoration"
+				container : "bottomDecoration"
 			}, this.transparencyRule);
 			console.log('startup');
 		},
@@ -54,17 +60,20 @@ define([
 		onOpen : function () {
 			console.log('onOpen');
 			/*var style = {
-				left : '100px',
-				right : '250px',
-				top : '150px',
-				bottom : '40px',
-				width : '80px'
+			left : '100px',
+			right : '250px',
+			top : '150px',
+			bottom : '40px',
+			width : '80px'
 			};
 			domStyle.set(this.sliderNode, style);*/
 		},
-
 		onClose : function () {
 			console.log('onClose');
+		},
+		_onTransparencyChanged : function (opacity) {
+			this._vectorial.setOpacity(1 - opacity);
+			this._baseAerea.setOpacity(opacity);
 		}
 
 	});
